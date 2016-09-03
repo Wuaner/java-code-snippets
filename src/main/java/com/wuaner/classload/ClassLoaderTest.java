38dbd31e53455625b41decffeae97d88733e3a7f
Wuaner <wuaner@gmail.com>
2016-09-03 11:23:16 +0800

    formatting

--- src/main/java/com/wuaner/classload/ClassLoaderTest.java
+++ src/main/java/com/wuaner/classload/ClassLoaderTest.java
@@ -1,4 +1,5 @@
 package com.wuaner.classload;
+
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -7,48 +8,48 @@ import sun.net.spi.nameservice.dns.DNSNameService;
 
 
 public class ClassLoaderTest {
-	
-	public static final String PROP_JAVA_CLASS_PATH = "java.class.path";
-	public static final String PROP_BOOT_CLASS_PATH = "sun.boot.class.path";
-	public static final String PROP_EXT_DIR = "java.ext.dirs";
 
-	public static final String SPLITTER = "=============================================================================================";
-	
-	public static void main( String[] args ) {
-		
-		printPath(PROP_JAVA_CLASS_PATH); // Could be wrong if run this in IntelliJ
-		printPath(PROP_BOOT_CLASS_PATH);
-		printPath(PROP_EXT_DIR);
+    public static final String PROP_JAVA_CLASS_PATH = "java.class.path";
+    public static final String PROP_BOOT_CLASS_PATH = "sun.boot.class.path";
+    public static final String PROP_EXT_DIR = "java.ext.dirs";
 
-		System.out.println("\n\n\n");
-		
-		printClassLoaderFor(String.class);
-		printClassLoaderFor(DNSNameService.class);
-		printClassLoaderFor(ClassLoaderTest.class);
+    public static final String SPLITTER = "=============================================================================================";
 
-       System.out.println("\n\n\n");
-       
-       ClassLoader cl = ClassLoader.getSystemClassLoader();
-       URL[] urls = ((URLClassLoader)cl).getURLs();
-       System.out.println("URLs of ClassLoader.getSystemClassLoader(count:" + urls.length + "):");
-       for(URL url: urls){
-    	   System.out.println(url.getFile());
+    public static void main(String[] args) {
+
+        printPath(PROP_JAVA_CLASS_PATH); // Could be wrong if run this in IntelliJ
+        printPath(PROP_BOOT_CLASS_PATH);
+        printPath(PROP_EXT_DIR);
+
+        System.out.println("\n\n\n");
+
+        printClassLoaderFor(String.class);
+        printClassLoaderFor(DNSNameService.class);
+        printClassLoaderFor(ClassLoaderTest.class);
+
+        System.out.println("\n\n\n");
+
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+        URL[] urls = ((URLClassLoader) cl).getURLs();
+        System.out.println("URLs of ClassLoader.getSystemClassLoader(count:" + urls.length + "):");
+        for (URL url : urls) {
+            System.out.println(url.getFile());
         }
     }
-	
-	private static void printPath(String propPath) {
-		String property = System.getProperty(propPath);
-		System.out.println(" Path of " + propPath + " (count:" + property.split(File.pathSeparator).length + "):");
-		for (String path : property.split(File.pathSeparator)) {
-			System.out.println(path);
-		}
-		
-		System.out.println(SPLITTER);
-	}
-	
-	private static void printClassLoaderFor(Class<?> clazz) {
-		System.out.println("ClassLoader for " + clazz.getName() + ":\n" + clazz.getClassLoader());
-		System.out.println(SPLITTER);
-	}
+
+    private static void printPath(String propPath) {
+        String property = System.getProperty(propPath);
+        System.out.println(" Path of " + propPath + " (count:" + property.split(File.pathSeparator).length + "):");
+        for (String path : property.split(File.pathSeparator)) {
+            System.out.println(path);
+        }
+
+        System.out.println(SPLITTER);
+    }
+
+    private static void printClassLoaderFor(Class<?> clazz) {
+        System.out.println("ClassLoader for " + clazz.getName() + ":\n" + clazz.getClassLoader());
+        System.out.println(SPLITTER);
+    }
 
 }
